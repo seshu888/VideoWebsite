b6c2d0ded776684b2b12dbfbbb595e2d2b0bbb28
seshu888 <seshukothapalli302@gmail.com>
2022-03-08 00:42:41 +0530

    tes54

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
 				<div className="cards-wrapper">
 					<ul className="cards-items">
 						<li className="card-item-container">
-							<img src={require('../images/img-9.jpg')} className="card-img" />
+							<img src='../images/img-9.jpg' className="card-img" />
 							<div className="content-section">
 								<p className="para">Travel through the great places in thsi workd </p>
 							</div>
 						</li>
 						<li className="card-item-container">
-							<img src={require('../images/img-9.jpg')} className="card-img" />
+							<img src='../images/img-9.jpg' className="card-img" />
 							<div className="content-section">
 								<p className="para">Travel through the great places in thsi workd </p>
 							</div>
@@ -24,19 +24,19 @@ function Cards() {
 					</ul>
 					<ul className="cards-items">
 						<li className="card-item-container">
-							<img src={require('../images/img-5.jpg')} className="card-img" />
+							<img src='../images/img-5.jpg' className="card-img" />
 							<div className="content-section">
 								<p className="para">Travel through the great places in thsi workd </p>
 							</div>
 						</li>
 						<li className="card-item-container">
-							<img src={require('../images/img-3.jpg')} className="card-img" />
+							<img src='../images/img-3.jpg' className="card-img" />
 							<div className="content-section">
 								<p className="para">Travel through the great places in thsi workd </p>
 							</div>
 						</li>
 						<li className="card-item-container">
-							<img src={require('../images/img-4.jpg')} className="card-img" />
+							<img src='../images/img-4.jpg' className="card-img" />
 							<div className="content-section">
 								<p className="para">Travel through the great places in thsi workd </p>
 							</div>
