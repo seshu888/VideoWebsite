0e7942de0661eda07ca26064fdb57f9f179f77c1
seshu888 <seshukothapalli302@gmail.com>
2022-03-08 00:56:16 +0530

    tes54

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ const HeroSection =()=>{
         <div className="hero-container">
            <video src={require("../videos/video-2.mp4")} autoPlay loop muted/>
      
-           <h1 className="title">ADVENTURE AWAITS</h1>
+           <h1 className="title">I LOVE YOU SRAVANI</h1>
            <p className="des">what are you waiting for ?</p>
            <div className="hero-btns">
             <button className="button1">GET STARTED</button>
