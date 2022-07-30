29e14d2c242725775a00c29eb5b5b72c91733270
seshu888 <seshukothapalli302@gmail.com>
2022-07-30 23:03:50 +0530

    first commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,19 +1,18 @@
-import React from "react";
-import './hero.css'
-const HeroSection =()=>{
-    return(
-        <div className="hero-container">
-           <video src={require("../videos/video-2.mp4")} autoPlay loop muted/>
-     
-           <h1 className="title">I LOVE YOU SRAVANI</h1>
-           <p className="des">what are you waiting for ?</p>
-           <div className="hero-btns">
-            <button className="button1">GET STARTED</button>
-            <button className="button2">WATCH TRAILER</button>
-            </div>
-     
-        </div>
-    )
-}
+import React from 'react';
+import './hero.css';
+const HeroSection = () => {
+	return (
+		<div className="hero-container">
+			<video src={require('../videos/video-2.mp4')} autoPlay loop muted />
 
-export default HeroSection
+			<h1 className="title">I LOVE YOU SRAVANI</h1>
+			<p className="des">what are you waiting for ?</p>
+			<div className="hero-btns">
+				<button className="button1">GET STARTED</button>
+				<button className="button2">WATCH TRAILER</button>
+			</div>
+		</div>
+	);
+};
+
+export default HeroSection;
